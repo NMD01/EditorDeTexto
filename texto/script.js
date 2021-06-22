c2f31df47b6d49fefdc39a104ec854550a97c514
Nathan Martini Duarte <77181059+NMD01@users.noreply.github.com>
2021-06-21 22:50:36 -0300

    Add files via upload

---
+++ texto/script.js
@@ -0,0 +1,15 @@
+let res = document.getElementById("res")
+let negrito = document.getElementById("negrito")
+
+negrito.addEventListener("click", bold = function(){
+    let  texto1 = document.getElementById("texto").value
+    
+    let selecionado = window.getSelection()
+    
+    
+    
+        //texto = `<strong>${selecionado}</strong>`
+        //res.innerHTML = texto
+    
+    
+})
